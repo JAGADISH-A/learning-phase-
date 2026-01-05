2d666a5c38e34e2e8e802be293e234c77a9c20ed
JAGADISH-A <jagadishchandirabosea@gmail.com>
2026-01-05 18:16:10 +0530

    FEAT : testing the merge conflicts

--- backend.java
+++ backend.java
@@ -4,8 +4,10 @@ public class backend
     public static void main(String[] args)
     {
         Scanner sc = new Scanner(System.in);
+        int roll = sc.nextInt();
         String name = sc.nextLine();
         System.out.println(name);
+        System.out.println(roll);
 
     }
 }
